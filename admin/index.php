c11da81ad374499a77b4f0a5efe4990398df027f
sekretarisdinsos75-arch <sekretarisdinsos75@gmail.com>
2025-11-19 18:04:59 +0700

    Add files via upload

---
+++ admin/index.php
@@ -0,0 +1,24 @@
+
+<?php include 'page web/header.php'; ?>    
+		<!-- BEGIN #content -->
+		<div id="content" class="app-content"> 
+		 
+                             <div class="content">
+        <div class="container-fluid">
+          <div class="card">
+            <div class="card-header card-chart card-header-danger"> 
+				
+			   <div class="content">
+        <div class="container-fluid">
+          <div class="card">
+            <div class="card-header card-chart card-header-danger"> 
+              </div>
+            <div class="col">
+                    <div class="logo-small">
+                        <img src="assets/images/berakhlak11.png" alt="" >
+            <center>  <h5 style="font-size:50px; font-family: Tahoma; color:DarkGreen;">MANAJEMEN DATA TERPADU LAYANAN PIMPINAN</h5></center> 
+            <center>  <h5 style="font-size:50px; font-family: Tahoma; color:DarkGreen;">(MANDAT-PIM)</h5></center> 
+                    </div>
+                </div> 
+
+          <?php include 'page web/foother.php'; ?>  
